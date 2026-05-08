b6d297379285370ad3c8c977c106d202ad0365e8
cocokalaco <pikakcoco@outlook.com>
2026-05-08 21:12:45 +0800

    3660

--- q3660/Q3660.java
+++ q3660/Q3660.java
@@ -9,12 +9,10 @@ public class Q3660 {
 
         int[] res = new int[length];
 
-        int maxVal = Integer.MIN_VALUE;
         int[] dpLeftMax = new int[length];
         dpLeftMax[0] = nums[0];
         for (int i = 1; i < length; i++) {
             dpLeftMax[i] = Math.max(dpLeftMax[i - 1], nums[i]);
-            maxVal = Math.max(maxVal, nums[i]);
         }
 
         // 从右向左看, 最右侧的第一个元素永远能取到以他为起点向左最大的元素.i+1是i右侧的数字, 通过跳转到右侧最小的数字位置, 再跳到i+1, 所以i+1能取到最大的数字也是i能取到的最大的数字.
